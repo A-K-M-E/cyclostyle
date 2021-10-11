78a2bb3105249448a5a45ff0802054c9993216e9
lo-bo-retta <92088317+lo-bo-retta@users.noreply.github.com>
2021-10-11 12:58:46 +0200

    Slider per create flyer
    :-D

--- js/custom-jquery.js
+++ js/custom-jquery.js
@@ -22,26 +22,56 @@ $( document ).ready(function() {
         $('#page-create').show(2000);
   });
   // menu footer create a flyer
-    $('.menu-create .nav-link').click(function(){
-        var target = $(this).data('target');
-        $('.panel').hide();
-        $('.menu-create .active').removeClass( "active" );
-        $(this).addClass( "active" );
-        $('#'+target).show();
-    });
+  //  $('.menu-create .nav-link').click(function(){
+  //      var target = $(this).data('target');
+  //      $('.panel').hide();
+  //      $('.menu-create .active').removeClass( "active" );
+  //      $(this).addClass( "active" );
+  //      $('#'+target).show();
+  //  });
       $('#preview').click(function(){
-        $('.panel').hide();
+        $('.carousel').hide();
         $('.menu-create').hide();
         $('.text-end').show();
         $('#flyer').show();
-        $('.menu-create .active').removeClass( "active" );
       });
       $('#back').click(function(){
-        $('#panel-template').show();
+        $('.carousel').show();
         $('.menu-create').show();
         $('.text-end').hide();
-        $('.menu-create .first').addClass( "active" );
         $('#flyer').hide();
       });
 
+      $('#carousel-content').on('slid.bs.carousel', function (e) {
+        $('.carousel-indicators .nav-link').removeClass('active')
+        var index = $(e.target).find('.active').index();
+        $('.carousel-indicators .nav-link').eq(index).addClass('active');
+      })
+
+      // var $carousel = $('#carousel-content');
+      // $carousel.carousel();
+      // var handled=false;//global variable
+
+      // $carousel.on('slide.bs.carousel', function (e) {
+        //   var current=$(e.target).find('.carousel-item.active');
+        //   var indx=$(current).index();
+        //   if((indx+2)>$('.carousel-indicators .nav-link').length)
+        //       indx=-1
+        //    if(!handled)
+        //    {
+        //       $('.carousel-indicators .nav-link').removeClass('active')
+        //       $('.carousel-indicators .nav-link:nth-child('+(indx+2)+')').addClass('active');
+        //    }
+        //    else
+        //    {
+        //       handled=!handled;//if handled=true make it back to false to work normally.
+        //    }
+    //   });
+
+      $(".carousel-indicators .nav-link").on('click',function(){
+         //Click event for indicators
+         $(this).addClass('active').siblings().removeClass('active');
+         //remove siblings active class and add it to current clicked item
+         handled=true; //set global variable to true to identify whether indicator changing was handled or not.
+      });
 });
